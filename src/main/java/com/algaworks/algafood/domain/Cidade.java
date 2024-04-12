da477a20d8f9077f93f9f8feb562c995d0775552
Diego de Almeida Ribeiro <diego.dev.ribeiro@gmail.com>
2024-04-12 07:48:32 -0300

    5.5

---
+++ src/main/java/com/algaworks/algafood/domain/Cidade.java
@@ -0,0 +1,30 @@
+package com.algaworks.algafood.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Cidade {
+
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private String nome;
+
+	@ManyToOne
+	@Column(nullable = false)
+	private Estado estado;
+
+}
---
+++ src/main/java/com/algaworks/algafood/domain/Cozinha.java
@@ -0,0 +1,5 @@
+package com.algaworks.algafood.domain;
+
+public class Cozinha {
+
+}
