ddb86e3b33d783250c1daea948512452e5e8d3c9
Diego de Almeida Ribeiro <diego.dev.ribeiro@gmail.com>
2024-04-13 22:49:38 -0300

    5.5

--- src/main/java/com/algaworks/algafood/domain/Permissao.java
+++ src/main/java/com/algaworks/algafood/domain/Permissao.java
@@ -1,5 +1,25 @@
 package com.algaworks.algafood.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Permissao {
 
+	@Id
+	@EqualsAndHashCode.Include
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String descricao;
+	
 }
