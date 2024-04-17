3cfad683a2fdcf2fd2fbd7992c878a6094e3becf
Diego de Almeida Ribeiro <diego.dev.ribeiro@gmail.com>
2024-04-16 22:13:56 -0300

    5.5

---
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroEstadoService.java
@@ -0,0 +1,35 @@
+package com.algaworks.algafood.domain.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Service;
+
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Estado;
+import com.algaworks.algafood.domain.repository.EstadoRepository;
+
+@Service
+public class CadastroEstadoService {
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
+	public Estado salvar(Estado estado) {
+		return estadoRepository.save(estado);
+	}
+
+	public void excluir(Long estadoId) {
+		try {
+			estadoRepository.deleteById(estadoId);
+		} catch (EmptyResultDataAccessException e) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("O estado informado pelo id %d não existe.", estadoId));
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(String
+					.format("O estado informado pelo id %d não pode ser excluído, pois ele está em uso.", estadoId));
+		}
+	}
+
+}
