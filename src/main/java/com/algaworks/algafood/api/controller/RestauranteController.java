23747245d8d77375b6b9526d3b49968fdb80e5eb
Diego de Almeida Ribeiro <diego.dev.ribeiro@gmail.com>
2024-04-16 23:55:27 -0300

    5.5

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -1,5 +1,42 @@
 package com.algaworks.algafood.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+
+@RestController
+@RequestMapping("/restaurantes")
 public class RestauranteController {
 
+	@Autowired
+	private RestauranteRepository restauranteRepository;
+	
+	@Autowired
+	private CadastroRestauranteService cadastroRestauranteService;
+	
+	@GetMapping
+	public List<Restaurante> listar() {
+		return restauranteRepository.findAll();
+	}
+	
+	@GetMapping("/{restauranteId}")
+	public ResponseEntity<Restaurante> buscar(@PathVariable Long restauranteId) {
+		Optional<Restaurante> restaurante = restauranteRepository.findById(restauranteId);
+		if (restaurante.isPresent()) {
+			return ResponseEntity.ok(restaurante.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	
 }
--- src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -1,9 +1,15 @@
 package com.algaworks.algafood.domain.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 
 @Service
@@ -11,8 +17,32 @@ public class CadastroRestauranteService {
 
 	@Autowired
 	private RestauranteRepository restauranteRepository;
-	
-	public Restaurante salvar (Restaurante restaurante) {
-		return restauranteRepository.save(restaurante);
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+
+	public Restaurante salvar(Restaurante restaurante) {
+		Long cozinhaId = restaurante.getCozinha().getId();
+		Cozinha cozinha = cozinhaRepository.findById(cozinhaId).orElseThrow(() -> {
+			throw new EntidadeNaoEncontradaException(
+					String.format("A cozinha informada pelo id %d não existe.", cozinhaId));
+		});
+		restaurante.setCozinha(cozinha);
+		restaurante = restauranteRepository.save(restaurante);
+
+		return restaurante;
+
+	}
+
+	public void excluir(Long restauranteId) {
+		try {
+			restauranteRepository.deleteById(restauranteId);
+		} catch (EmptyResultDataAccessException e) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("O restaurante informado pelo id %d não existe.", restauranteId));
+		} catch (DataIntegrityViolationException e) {
+			throw new EntidadeEmUsoException(String.format(
+					"O restaurante informado pelo id %d não pode ser excluído, pois ele está em uso.", restauranteId));
+		}
 	}
 }
