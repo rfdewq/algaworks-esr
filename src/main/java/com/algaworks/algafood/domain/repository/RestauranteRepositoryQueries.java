ce142a429a936e866ad2d10ae7328385fdf66342
Diego de Almeida Ribeiro <diego.dev.ribeiro@gmail.com>
2024-04-21 21:28:09 -0300

    5.20

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepositoryQueries.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepositoryQueries.java
@@ -7,7 +7,7 @@ import com.algaworks.algafood.domain.model.Restaurante;
 
 public interface RestauranteRepositoryQueries {
 	
-	public List<Restaurante> find (String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal);
+	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal);
 	
 	public List<Restaurante> findComFreteGratis(String nome);
 }
